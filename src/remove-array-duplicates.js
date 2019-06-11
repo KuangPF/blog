71c7cabdca4288276b4ef85b0b52104870c1df9d
KuangPF <1633397286@qq.com>
2019-06-11 10:16:03 +0800

    feat: remove array duplicates with filter

--- src/remove-array-duplicates.js
+++ src/remove-array-duplicates.js
@@ -1,8 +1,22 @@
 const array = [1, 2, 3, 4, 2, 34, 53, 2]
 
 var arrayUnique = array.reduce((unique, item) => {
-  console.log(unique)
   return unique.includes(item) ? unique : [...unique, item]
 }, [])
 
-console.log(arrayUnique)
+// console.log(arrayUnique)
+
+var unique = function(array) {
+  return array.filter(function(value, index) {
+    return array.indexOf(value) === index
+  })
+}
+
+const uniqueES6 = array => {
+  return array.filter((value, index) => {
+    return array.indexOf(value) === index
+  })
+}
+
+console.log(unique(array))
+console.log(uniqueES6(array))
