1c408f91d0ebe547aa27c79b6776c2fcba0bf037
KuangPF <1633397286@qq.com>
2019-06-13 14:58:29 +0800

    docs: update

---
+++ src/Understanding-ECMAScript6/Functions/arrow-function.js
@@ -0,0 +1,13 @@
+var pageHander = {
+  id: 123456,
+  init: function() {
+    console.log(this.id)
+  }
+}
+
+function createArrowFunctionReturningFirstArgument() {
+  return ()=> arguments[0]
+}
+
+var arrowFunction = createArrowFunctionReturningFirstArgument(5)
+console.log(arrowFunction())
